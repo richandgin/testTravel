632e80d7df37c647bed0f9c05c1c8b54ecea5c58
Superman <superman@gmail.com>
2025-03-11 23:31:04 -0400

    Got Step 1 working!

--- travel_recommendation7.js
+++ travel_recommendation7.js
@@ -6,12 +6,19 @@ fetch('./travel_recommendation_api.json')
     const places = data[0].countries;
     const all = data;
     console.log ('Data 0:',data[0].countries[1])
+    
     const allcountries = data[0].countries;
     console.log ('All countries:',allcountries);
+    
+    const alltemples = data[0].temples;
+    console.log ('All Temples:',alltemples);
+
+    const allbeaches = data[0].beaches;
+    console.log ('All Beaches:',allbeaches);
 
     const foundcities = allcountries
         .filter(city => city.name === 'Japan');
-    console.log ('City =', foundcities);
+    console.log ('Searched City =', foundcities);
 
     // Loop through the places and log their names
     console.log('Countries =', places);
