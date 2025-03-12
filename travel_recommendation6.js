66c8f6e7af0a1dc61f30d1999cef463733c28f00
Superman <superman@gmail.com>
2025-03-11 22:38:10 -0400

    Narrowing down parsing multilevel json arrays

--- travel_recommendation6.js
+++ travel_recommendation6.js
@@ -1,14 +1,14 @@
-fetch('./travel_recommendation_api3.json')
+fetch('./travel_recommendation_api2.json')
   .then(response => response.json())
   .then(data => {
     // Access the array of places
     console.log ('data = ', data);
-    const places = data.places;
+    const places = data[1].cities[0].name[0];
 
     // Loop through the places and log their names
     console.log('Countries =', places);
     places.forEach(place => {
-      console.log(`Place: ${place.name}, Country: ${place.country}`);
+      console.log(`Place: ${place.name}, Country: ${place.description}`);
     });
   })
   .catch(error => console.error('Error fetching the JSON file:', error));
