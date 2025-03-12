970ff7e57f74c2966738bea0da930def757683bf
Superman <superman@gmail.com>
2025-03-11 23:24:40 -0400

    JS7 seems to work

--- travel_recommendation7.js
+++ travel_recommendation7.js
@@ -1,20 +1,22 @@
-fetch('./travel_recommendation_api2.json')
+fetch('./travel_recommendation_api.json')
   .then(response => response.json())
   .then(data => {
     // Access the array of places
     console.log ('data = ', data);
-    const places = data[0].cities;
+    const places = data[0].countries;
     const all = data;
-    console.log ('Data 0:',data[0].cities[1])
+    console.log ('Data 0:',data[0].countries[1])
+    const allcountries = data[0].countries;
+    console.log ('All countries:',allcountries);
 
-    const foundcities = data
+    const foundcities = allcountries
         .filter(city => city.name === 'Japan');
     console.log ('City =', foundcities);
 
     // Loop through the places and log their names
     console.log('Countries =', places);
     places.forEach(place => {
-      console.log(`Place: ${place.name}, Country: ${place.description}`);
+      console.log(`Place: ${place.name}, Country: ${place.cities}`);
     });
   })
   .catch(error => console.error('Error fetching the JSON file:', error));
