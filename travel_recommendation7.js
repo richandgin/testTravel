882b896679f75783e42d6154540df2c7e03ad2df
Superman <superman@gmail.com>
2025-03-14 00:46:24 -0400

    It's working!

--- travel_recommendation7.js
+++ travel_recommendation7.js
@@ -1,29 +1,39 @@
-fetch('./travel_recommendation_api.json')
-  .then(response => response.json())
-  .then(data => {
-    // Access the array of places
-    console.log ('data = ', data);
-    const places = data[0].countries;
-    const all = data;
-    console.log ('Data 0:',data[0].countries[1])
-    
-    const allcountries = data[0].countries;
-    console.log ('All countries:',allcountries);
-    
-    const alltemples = data[0].temples;
-    console.log ('All Temples:',alltemples);
+function cmdSearch() {
+    var searchStr = document.getElementById('conditionInput').value;
+    /*lcSearchStr = searchStr.toLowerCase();*/
+    alert('Search for: ' + searchStr);
 
-    const allbeaches = data[0].beaches;
-    console.log ('All Beaches:',allbeaches);
+    fetch('./travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+        // Access the array of places
+        console.log ('data = ', data);
+        const places = data[0].countries;
+        const all = data;
+        console.log ('Data 0:',data[0].countries[1])
+        
+        const allcountries = data[0].countries;
+        console.log ('All Countries:',allcountries);
+        
+        const alltemples = data[0].temples;
+        console.log ('All Temples:',alltemples);
 
-    const foundcities = allcountries
-        .filter(city => city.name === 'Japan');
-    console.log ('Searched City =', foundcities);
+        const allbeaches = data[0].beaches;
+        console.log ('All Beaches:',allbeaches);
 
-    // Loop through the places and log their names
-    console.log('Countries =', places);
-    places.forEach(place => {
-      console.log(`Place: ${place.name}, Country: ${place.cities}`);
-    });
-  })
-  .catch(error => console.error('Error fetching the JSON file:', error));
+        /*const foundcities = allcountries
+            .filter(city => city.name === 'Japan');
+        console.log ('Searched City =', foundcities);*/
+
+        const foundcities2 = allcountries
+            .filter(city => {return city.name.toLowerCase().startsWith(searchStr.toLowerCase())});
+        console.log ('Found Cities2: ', foundcities2)
+
+        // Loop through the places and log their names
+        console.log('Countries =', places);
+        places.forEach(place => {
+        console.log(`Place: ${place.name}, Country: ${place.cities}`);
+        });
+    })
+    .catch(error => console.error('Error fetching the JSON file:', error));
+};
